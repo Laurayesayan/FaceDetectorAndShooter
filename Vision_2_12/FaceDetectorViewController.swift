bce80fec40f2656d4d272a502661e0cb5b77ebc0
laurayesayan <laurayesayan@gmail.com>
2020-04-20 23:03:17 +0300

    Fixed difference between coordinate spaces.

--- Vision_2_12/FaceDetectorViewController.swift
+++ Vision_2_12/FaceDetectorViewController.swift
@@ -75,14 +75,12 @@ class FaceDetectorViewController: ViewController {
     }
     
     func presentAlert(_ title: String, error: NSError) {
-        // Always present alert on main thread.
         DispatchQueue.main.async {
             let alertController = UIAlertController(title: title,
                                                     message: error.localizedDescription,
                                                     preferredStyle: .alert)
             let okAction = UIAlertAction(title: "OK",
                                          style: .default) { _ in
-                                            // Do nothing -- simply dismiss alert.
             }
             alertController.addAction(okAction)
             self.present(alertController, animated: true, completion: nil)
@@ -105,48 +103,15 @@ class FaceDetectorViewController: ViewController {
         }
     }
     
-    fileprivate func boundingBox(forRegionOfInterest: CGRect, withinImageBounds bounds: CGRect) -> CGRect {
-        
-        let imageWidth = bounds.width
-        let imageHeight = bounds.height
-        
-        // Begin with input rect.
-        var rect = forRegionOfInterest
-        
-        // Reposition origin.
-        rect.origin.x *= imageWidth
-        rect.origin.x += bounds.origin.x
-        rect.origin.y = (1 - rect.origin.y) * imageHeight + bounds.origin.y
-        
-        // Rescale normalized coordinates.
-        rect.size.width *= imageWidth
-        rect.size.height *= imageHeight
-        
-        return rect
-    }
-    
-    fileprivate func shapeLayer(color: UIColor, frame: CGRect) -> CAShapeLayer {
-        // Create a new layer.
-        let layer = CAShapeLayer()
-        
-        // Configure layer's appearance.
-        layer.fillColor = nil // No fill to show boxed object
-        layer.shadowOpacity = 0
-        layer.shadowRadius = 0
-        layer.borderWidth = 2
-        
-        // Vary the line color according to input.
-        layer.borderColor = color.cgColor
-        
-        // Locate the layer.
-        layer.anchorPoint = .zero
-        layer.frame = frame
-        layer.masksToBounds = true
-        
-        // Transform the layer to have same coordinate system as the imageView underneath it.
-        layer.transform = CATransform3DMakeScale(1, -1, 1)
-        
-        return layer
+    func createRoundedRectLayerWithBounds(_ bounds: CGRect) -> CALayer {
+        let shapeLayer = CALayer()
+        shapeLayer.bounds = bounds
+        shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        shapeLayer.name = "Found Object"
+        shapeLayer.borderWidth = 2
+        shapeLayer.borderColor = #colorLiteral(red: 0.9849896891, green: 0.8375290671, blue: 0.3429784259, alpha: 0.7966074486)
+        shapeLayer.cornerRadius = 7
+        return shapeLayer
     }
     
     fileprivate func draw(faces: [VNFaceObservation], onImageWithBounds bounds: CGRect) {
@@ -154,8 +119,8 @@ class FaceDetectorViewController: ViewController {
         pathLayer.sublayers = nil
         if animationCompleted {
             for observation in faces {
-                let faceBox = boundingBox(forRegionOfInterest: observation.boundingBox, withinImageBounds: bounds)
-                let faceLayer = shapeLayer(color: .yellow, frame: faceBox)
+                let faceBox = VNImageRectForNormalizedRect(observation.boundingBox, Int(bufferSize.width), Int(bufferSize.height))
+                let faceLayer = createRoundedRectLayerWithBounds(faceBox)
 
                 animateShot(to: faceBox)
                 pathLayer.addSublayer(faceLayer)
@@ -167,23 +132,25 @@ class FaceDetectorViewController: ViewController {
     fileprivate func animateShot(to faceBox: CGRect) {
         let bullet = UIImageView(image: UIImage(imageLiteralResourceName: "bullet"))
         bullet.frame = CGRect(origin: CGPoint.zero, size: bullet.image!.size)
-        bullet.layer.anchorPoint = CGPoint(x: 0, y: 0)
+        bullet.layer.anchorPoint = CGPoint(x: 1, y: 0)
         bullet.center = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.height - bullet.image!.size.height)
-//        bullet.transform = CGAffineTransform(scaleX: 1, y: -1)
-        
+
+        let convertedFaceBox = pathLayer.convert(faceBox, to: view.layer)
         view.addSubview(bullet)
         
         let distance = view.frame.size.width / min(faceBox.size.width, faceBox.size.height)
-        let duration = Double(distance) * 0.45
-        
+        let duration = Double(distance) * 0.3
+
         UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { [weak self] in
-            bullet.center = CGPoint(x: faceBox.origin.y - faceBox.size.height / 2, y: faceBox.origin.x + faceBox.size.width / 2)
-//            bullet.center = CGPoint(x: self!.view.frame.midX, y: self!.view.frame.midY)
-//            bullet.center = CGPoint(x: faceBox.midY, y: faceBox.midX)
+            bullet.layer.position = CGPoint(x: convertedFaceBox.midX, y: convertedFaceBox.midY)
             self!.animationCompleted = false
-        }) { [weak self] (F) in
+        }) { [weak self] (bool) in
             bullet.isHidden = true
             self!.animationCompleted = true
         }
+    }
+    
+    func convertCoords(rect: CGRect) {
+        
     }
 }
